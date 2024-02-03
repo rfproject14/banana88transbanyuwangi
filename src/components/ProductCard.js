0c3db523a6e0f46839bdab46679ef1b29bcff338
rfproject14 <ferdyan.reza@gmail.com>
2024-02-03 15:49:12 +0700

    ...

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -43,7 +43,5 @@ export default function ProductCard({ image, title, price, details, href }) {
         </button>
       </div>
     </div>
-    <button type="button" className="px-5 py-2.5 text-sm font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Base</button>
-
   );
 }
